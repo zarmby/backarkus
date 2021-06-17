ce2bb1f29a1356ccc23449bb2cdb9a19c47510ea
Carlos Ivan Mercado Marin <karlos123@junhotmail.com>
2021-06-17 10:45:22 -0500

    rutas terminadas

--- server/routes/equipments.routes.js
+++ server/routes/equipments.routes.js
@@ -1,9 +1,41 @@
-const equipmentsmodel = require("../models/equipments.model");
+const equipmentmodel = require("../models/equipments.model");
 const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
   try {
+    const equipment = await equipmentmodel.find({ status: true });
+    idEquipment = req.query.idEquipment;
+    const equipmentfind = await equipmentmodel.findById(idEquipment);
+    if (equipmentfind) {
+      return res.status(400).json({
+        estatus: "200",
+        err: false,
+        msg: "Information obtained correctly.",
+        cont: {
+          name: equipmentfind,
+        },
+      });
+    }
+    if (equipment.length <= 0) {
+      res.status(404).send({
+        estatus: "404",
+        err: true,
+        msg: "No equipments were found in the database.",
+        cont: {
+          equipment,
+        },
+      });
+    } else {
+      res.status(200).send({
+        estatus: "200",
+        err: false,
+        msg: "Information obtained correctly.",
+        cont: {
+          equipment,
+        },
+      });
+    }
   } catch (err) {
     res.status(500).send({
       estatus: "500",
@@ -18,6 +50,51 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   try {
+    const equipment = new equipmentmodel(req.body);
+    let err = equipment.validateSync();
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        resp: 400,
+        msg: "Error: Error to insert equipment",
+        cont: {
+          err,
+        },
+      });
+    }
+    const equipmentfind = await equipmentmodel.findOne({
+      serialnumber: { $regex: `${equipment.serialnumber}$`, $options: "i" },
+    });
+    if (equipmentfind) {
+      return res.status(400).json({
+        ok: false,
+        resp: 400,
+        msg: "This equipment already exists",
+        cont: {
+          serialnumber: equipmentfind,
+        },
+      });
+    }
+    const newequipment = await equipment.save();
+    if (newequipment.length <= 0) {
+      res.status(400).send({
+        estatus: "400",
+        err: true,
+        msg: "Error: equipment could not be registered",
+        cont: {
+          newequipment,
+        },
+      });
+    } else {
+      res.status(200).send({
+        estatus: "200",
+        err: false,
+        msg: "Success: Information register correctly.",
+        cont: {
+          newequipment,
+        },
+      });
+    }
   } catch (err) {
     res.status(500).send({
       estatus: "500",
@@ -32,6 +109,59 @@ app.post("/", async (req, res) => {
 
 app.put("/", async (req, res) => {
   try {
+    const idEquipment = req.query.idEquipment;
+    if (req.query.idEquipment == "") {
+      return res.status(400).json({
+        estatus: "400",
+        err: true,
+        msg: "Error: A valid id was not sent",
+        cont: 0,
+      });
+    }
+    req.body._id = idEquipment;
+    const equipmentfind = await equipmentmodel.findById(idEquipment);
+    if (!equipmentfind) {
+      return res.status(400).json({
+        estatus: "404",
+        err: true,
+        msg: "Error: The equipment was not found in the database.",
+        cont: equipmentfind,
+      });
+    }
+    const newequipment = new equipmentmodel(req.body);
+    let err = newequipment.validateSync();
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        resp: 400,
+        msg: "Error: Error inserting equipment",
+        cont: {
+          err,
+        },
+      });
+    }
+    const equipmentupdate = await equipmentmodel.findByIdAndUpdate(
+      idEquipment,
+      { $set: newequipment },
+      { new: true }
+    );
+    if (!equipmentupdate) {
+      return res.status(400).json({
+        ok: false,
+        resp: 400,
+        msg: "Error: Trying to update the role",
+        cont: 0,
+      });
+    } else {
+      return res.status(400).json({
+        ok: true,
+        resp: 200,
+        msg: "Success: The equipment was update successfully.",
+        cont: {
+          equipmentfind,
+        },
+      });
+    }
   } catch (err) {
     res.status(500).send({
       estatus: "500",
@@ -46,6 +176,46 @@ app.put("/", async (req, res) => {
 
 app.delete("/", async (req, res) => {
   try {
+    if (req.query.idEquipment == "") {
+      return res.status(400).send({
+        estatus: "400",
+        err: true,
+        msg: "Error: A valid id was not sent.",
+        cont: 0,
+      });
+    }
+    idEquipment = req.query.idEquipment;
+    status = req.query.status;
+    const equipmentfind = await equipmentmodel.findById(idEquipment);
+    if (!equipmentfind) {
+      return res.status(404).send({
+        estatus: "404",
+        err: true,
+        msg: "Error: The user cannot be found in the database.",
+        cont: equipmentfind,
+      });
+    }
+    const equipmentupdate = await equipmentmodel.findByIdAndUpdate(
+      idEquipment,
+      { $set: { status } },
+      { new: true }
+    );
+    if (!equipmentupdate) {
+      return res.status(400).json({
+        ok: false,
+        resp: 400,
+        msg: "Error:: when trying to delete the equipment.",
+        cont: 0,
+      });
+    } else {
+      return res.status(200).json({
+        ok: true,
+        resp: 200,
+        msg: `Success: se a ${
+          status === "true" ? "Activate" : " Delete"
+        } the equipment`,
+      });
+    }
   } catch (err) {
     res.status(500).send({
       estatus: "500",
